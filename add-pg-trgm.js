d668aa837d47a1b2707a60cb1ee0722c5718d6a6
aravind45 <37077333+aravind45@users.noreply.github.com>
2025-12-24 20:25:33 -0600

    Remove API keys and database credentials from files for security

--- add-pg-trgm.js
+++ add-pg-trgm.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 async function addPgTrgmExtension() {
   const client = new Client({
-    connectionString: 'postgresql://neondb_owner:npg_EJaq0VAklM4o@ep-green-bar-ahj4m4hv-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+    connectionString: process.env.DATABASE_URL || 'postgresql://user:password@localhost:5432/database'
   });
 
   try {
